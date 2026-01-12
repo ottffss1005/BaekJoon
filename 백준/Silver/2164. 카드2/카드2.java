ff2ffdc18846f156ecc1e53608a0ab31d9ee36e0
So Young Choi <62324475+ottffss1005@users.noreply.github.com>
2026-01-12 15:29:35 +0900

    [Silver IV] Title: 카드2, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,5 +1,6 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,15 +8,15 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i=0; i < N; i++){
-            queue.add(i+1);
+        int power = Integer.highestOneBit(N);
+
+        int answer;
+        if (N == power) {
+            answer = N;
+        } else {
+            answer = 2 * (N - power);
         }
 
-        while(queue.size()>1){
-            queue.poll();
-            queue.add(queue.poll());
-        }
-        System.out.println(queue.poll());
+        System.out.println(answer);
     }
 }
