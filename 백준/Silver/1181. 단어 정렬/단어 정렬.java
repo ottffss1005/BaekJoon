fcbd759c52a264bbb43d784e43ae28ad3287d1bb
So Young Choi <62324475+ottffss1005@users.noreply.github.com>
2026-01-12 14:46:10 +0900

    [Silver V] Title: 단어 정렬, Time: 392 ms, Memory: 22420 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            if (a.length() != b.length()) {
+                return a.length() - b.length();
+            }
+            return a.compareTo(b);
+        });
+
+        String prev = null;
+
+        for (String str : arr) {
+            if (!str.equals(prev)) {
+                System.out.println(str);
+                prev = str;
+            }
+        }
+    }
+}
